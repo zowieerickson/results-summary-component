b88e2297fff0a19a3a837b25e7c8a2bbdda451b6
Zowie Erickson <zowie.erickson@gmail.com>
2023-06-19 10:09:12 -0400

    dynamically load data from JSON

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,67 +1,41 @@
 import '../styles/summary.css'
-import reactionLogo from '../assets/images/icon-reaction.svg'
-import memoryLogo from '../assets/images/icon-memory.svg'
-import verbalLogo from '../assets/images/icon-verbal.svg'
-import visualLogo from '../assets/images/icon-visual.svg'
 import Button from './Button'
+import SummaryData from '../data.json'
+
+
+export function SummaryCategory(props) {
+    let categoryClass = props.item.category.toLowerCase()
+
+    return (
+        <div className={`summary-category ${categoryClass}`}>
+            <div className="summary-category-title">
+                <img src={props.item.icon}
+                className="summary-category-image"
+                alt="" 
+                width="20px"
+                />
+                <p>{props.item.category}</p>
+            </div>
+            <div className="summary-score">
+                <p>{props.item.score} <span className="summary-score-max">/ 100</span></p>
+            </div>
+        </div>
+    )
+}
 
 export default function Summary() {
+    const summaryElements = SummaryData.map(item => {
+        return <SummaryCategory
+          key={item.id}
+          item={item}
+        />
+      })
+
     return (
        <section className="summary">
             <h2 className="summary-title">Summary</h2>
             <div className='summary-categories'>
-                <div className="summary-category reaction">
-                    <div className="summary-category-title">
-                        <img src={reactionLogo}
-                        className="summary-category-image"
-                        alt="" 
-                        width="20px"
-                        />
-                        <p>Reaction</p>
-                    </div>
-                    <div className="summary-score">
-                        <p>80 <span className="summary-score-max">/ 100</span></p>
-                    </div>
-                </div>
-                <div className="summary-category memory">
-                    <div className="summary-category-title">
-                        <img src={memoryLogo}
-                        className="summary-category-image"
-                        alt="" 
-                        width="20px"
-                        />
-                        <p>Memory</p>
-                    </div>
-                    <div className="summary-score">
-                        <p>92 <span className="summary-score-max">/ 100</span></p>
-                    </div>
-                </div>
-                <div className="summary-category verbal">
-                    <div className="summary-category-title">
-                        <img src={verbalLogo}
-                        className="summary-category-image"
-                        alt="" 
-                        width="20px"
-                        />
-                        <p>Verbal</p>
-                    </div>
-                    <div className="summary-score">
-                        <p>61 <span className="summary-score-max">/ 100</span></p>
-                    </div>
-                </div>
-                <div className="summary-category visual">
-                    <div className="summary-category-title">
-                        <img src={visualLogo}
-                        className="summary-category-image"
-                        alt="" 
-                        width="20px"
-                        />
-                        <p>Visual</p>
-                    </div>
-                    <div className="summary-score">
-                        <p>73 <span className="summary-score-max">/ 100</span></p>
-                    </div>
-                </div>
+                {summaryElements}
             </div>
             <Button>Continue</Button>
        </section>
