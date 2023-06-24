a47010073bfbd0f0731dce506c0543d8bdcb049a
Zowie Erickson <zowie.erickson@gmail.com>
2023-06-24 07:48:00 -0400

    update result score number to dynamically be the average of all the scores in summary section

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,21 @@
 import '../styles/result.css'
+import data from '../data.json'
 
 export default function Result() {
+    let scores = []
+    data.map(resultScore => scores.push(resultScore.score))
+
+    const averageScore = Math.floor(
+        scores.reduce( 
+            (a,b) => a + b, 0
+        ) / scores.length
+    )
+
     return (
         <section className="results">
             <h2 className="result-title light-blue">Your Result</h2>
             <div className="result-score">
-                <p className="result-score-exact">76</p>
+                <p className="result-score-exact">{averageScore}</p>
                 <p className="result-score-max light-blue">of 100</p>
             </div>
             <p className="result-assessment">Great</p>
